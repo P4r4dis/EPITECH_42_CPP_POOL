1e0100a1b94d0aa9c5ad4dc83bd4a1653561820e
P4r4dis <adil.d.pro@gmail.com>
2024-09-02 19:52:33 +0200

    [ADD] - unit test for reference string and main function added

--- CPP_01/42/M01/ex04/bin/main.cpp
+++ CPP_01/42/M01/ex04/bin/main.cpp
@@ -6,7 +6,7 @@
 /*   By: Paradis <adil.d.pro@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 21:12:48 by Paradis           #+#    #+#             */
-/*   Updated: 2024/09/02 19:42:58 by Paradis          ###   ########.fr       */
+/*   Updated: 2024/09/02 19:51:41 by Paradis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,16 @@
 int     main(void)
 {
     std::string str = "HI THIS IS BRAIN";
-
     std::string *stringPTR = &str;
-    std::cout   << "string: " << str << std::endl
-                << "stringPTR: " << *stringPTR << std::endl;
+    std::string &stringREF = str;
+
     std::cout   << "address string: " << &str << std::endl
-                << "address stringPTR: " << &stringPTR << std::endl;
+                << "address stringPTR: " << &stringPTR << std::endl
+                << "address stringREF: " << &stringREF << std::endl;
+
+    std::cout   << "string: " << str << std::endl
+                << "stringPTR: " << *stringPTR << std::endl
+                << "stringREF: " << stringREF << std::endl;
+
     return 0;
 }
--- CPP_01/42/M01/ex04/test/bin/test_main.cpp
+++ CPP_01/42/M01/ex04/test/bin/test_main.cpp
@@ -6,7 +6,7 @@
 /*   By: Paradis <adil.d.pro@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 17:20:59 by Paradis           #+#    #+#             */
-/*   Updated: 2024/09/02 19:41:19 by Paradis          ###   ########.fr       */
+/*   Updated: 2024/09/02 19:46:02 by Paradis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,19 +37,19 @@ Test(stringPTR, Test_pointer_to_the_string, .init = redirect_all_stdout)
     );
 }
 
-// Test(stringREF, Test_reference_to_the_string, .init = redirect_all_stdout)
-// {
-//     std::string str = "HI THIS IS BRAIN";
+Test(stringREF, Test_reference_to_the_string, .init = redirect_all_stdout)
+{
+    std::string str = "HI THIS IS BRAIN";
 
-//     std::string &stringREF = str;
-//     std::cout   << "string: " << str << std::endl
-//                 << "stringREF: " << stringREF << std::endl;
-//     cr_assert_stdout_eq_str
-//     (
-//         "string: HI THIS IS BRAIN\n"
-//         "stringREF: HI THIS IS BRAIN\n"
-//     );
-// }
+    std::string &stringREF = str;
+    std::cout   << "string: " << str << std::endl
+                << "stringREF: " << stringREF << std::endl;
+    cr_assert_stdout_eq_str
+    (
+        "string: HI THIS IS BRAIN\n"
+        "stringREF: HI THIS IS BRAIN\n"
+    );
+}
 
 Test(address, Test_address_pointer_to_the_string, .init = redirect_all_stdout)
 {
@@ -61,12 +61,12 @@ Test(address, Test_address_pointer_to_the_string, .init = redirect_all_stdout)
     cr_assert(ne(ptr, &str, &stringPTR));
 }
 
-// Test(address, Test_address_reference_to_the_string, .init = redirect_all_stdout)
-// {
-//     std::string str = "HI THIS IS BRAIN";
+Test(address, Test_address_reference_to_the_string, .init = redirect_all_stdout)
+{
+    std::string str = "HI THIS IS BRAIN";
 
-//     std::string &stringREF = str;
-//     std::cout   << "address string: " << &str << std::endl
-//                 << "address stringREF: " << &stringREF << std::endl;
-//     cr_assert(eq(ptr, &str, &stringREF));
-// }
+    std::string &stringREF = str;
+    std::cout   << "address string: " << &str << std::endl
+                << "address stringREF: " << &stringREF << std::endl;
+    cr_assert(eq(ptr, &str, &stringREF));
+}
