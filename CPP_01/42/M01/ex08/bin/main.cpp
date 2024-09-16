415a003d0cd8b5765f0ea30b53c5a4bb297c2e56
P4r4dis <adil.d.pro@gmail.com>
2024-09-16 20:11:48 +0200

    [ADD] - main function added

--- CPP_01/42/M01/ex08/bin/main.cpp
+++ CPP_01/42/M01/ex08/bin/main.cpp
@@ -6,11 +6,20 @@
 /*   By: Paradis <adil.d.pro@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 21:12:48 by Paradis           #+#    #+#             */
-/*   Updated: 2024/09/16 17:58:26 by Paradis          ###   ########.fr       */
+/*   Updated: 2024/09/16 20:10:07 by Paradis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/Human.hpp"
+
 int main(void)
 {
+    Human human;
+    std::string target = "Joe";
+
+    human.action("melee attack", target);
+    human.action("ranged attack", target);
+    human.action("intimidating shout", target);
+    human.action("magical attack", target);
     return 0;
 }
