a91ac823c97216a843092c376d9532ddc1f98fc5
P4r4dis <adil.d.pro@gmail.com>
2024-09-16 20:07:58 +0200

    [ADD] - unit test for Human::action, HUman::meleeAttack, Human::rangedAttack and human::intimidatingShout added

--- CPP_01/42/M01/ex08/source/Human.cpp
+++ CPP_01/42/M01/ex08/source/Human.cpp
@@ -6,7 +6,7 @@
 /*   By: Paradis <adil.d.pro@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 18:10:56 by Paradis           #+#    #+#             */
-/*   Updated: 2024/09/16 19:10:41 by Paradis          ###   ########.fr       */
+/*   Updated: 2024/09/16 19:50:48 by Paradis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,29 @@ void        Human::rangedAttack(std::string const & target)
 void        Human::intimidatingShout(std::string const & target)
 {
     std::cout << "Human launches his intimidating shout on " << target << std::endl;
+}
+
+void        Human::action(std::string const & action_name, std::string const & target)
+{
+    void    (Human::*functionArray[])(std::string const & target) = { 
+    &Human::meleeAttack,
+    &Human::rangedAttack,
+    &Human::intimidatingShout
+    };
+
+    std::string nameAction[] = { 
+        "melee attack",
+        "ranged attack", 
+        "intimidating shout"
+    };
+
+    int     sizeArray = sizeof(nameAction) / sizeof(nameAction[0]);
+    for (int i = 0; i < sizeArray; ++i)
+        if (nameAction[i] == action_name)
+        {
+            (this->*functionArray[i])(target);
+            return ;
+        }
+
+    std::cout << "Unknown action: " << action_name << std::endl;
 }
--- CPP_01/42/M01/ex08/test/bin/test_main.cpp
+++ CPP_01/42/M01/ex08/test/bin/test_main.cpp
@@ -6,7 +6,7 @@
 /*   By: Paradis <adil.d.pro@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 17:20:59 by Paradis           #+#    #+#             */
-/*   Updated: 2024/09/16 18:48:30 by Paradis          ###   ########.fr       */
+/*   Updated: 2024/09/16 19:59:00 by Paradis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,4 +27,60 @@ Test(Human, Test_Human_class_isDefined, .init = redirect_all_stdout)
 {
     Human human;
     cr_assert_not_null(&human);
+}
+
+Test(Human, Test_Human_action_isDefined, .init = redirect_all_stdout)
+{
+    Human human;
+    std::string target = "Joe";
+    
+    human.action("melee attack", target);
+}
+
+Test(Human, Test_Human_action_meleeAttack, .init = redirect_all_stdout)
+{
+    Human human;
+    std::string target = "Joe";
+    
+    human.action("melee attack", target);
+    cr_assert_stdout_eq_str
+    (
+        ("Human launches his melee attack on " + target + "\n").c_str()
+    );
+}
+
+Test(Human, Test_Human_action_rangedAttack, .init = redirect_all_stdout)
+{
+    Human human;
+    std::string target = "Joe";
+    
+    human.action("ranged attack", target);
+    cr_assert_stdout_eq_str
+    (
+        ("Human launches his ranged attack on " + target + "\n").c_str()
+    );
+}
+
+Test(Human, Test_Human_action_intimidatingShout, .init = redirect_all_stdout)
+{
+    Human human;
+    std::string target = "Joe";
+    
+    human.action("intimidating shout", target);
+    cr_assert_stdout_eq_str
+    (
+        ("Human launches his intimidating shout on " + target + "\n").c_str()
+    );
+}
+
+Test(Human, Test_Human_action_unknown_action_name, .init = redirect_all_stdout)
+{
+    Human human;
+    std::string target = "Joe";
+    
+    human.action("magical attack", target);
+    cr_assert_stdout_eq_str
+    (
+        "Unknown action: magical attack\n"
+    );
 }
