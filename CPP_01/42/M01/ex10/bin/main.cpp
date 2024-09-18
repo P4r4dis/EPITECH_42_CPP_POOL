d040a364f3bd5a40e89b57041343c2efb3fa6582
P4r4dis <adil.d.pro@gmail.com>
2024-09-18 20:58:15 +0200

    [ADD] - implementation of main function added

--- CPP_01/42/M01/ex10/bin/main.cpp
+++ CPP_01/42/M01/ex10/bin/main.cpp
@@ -6,13 +6,30 @@
 /*   By: Paradis <adil.d.pro@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 21:12:48 by Paradis           #+#    #+#             */
-/*   Updated: 2024/09/18 19:44:00 by Paradis          ###   ########.fr       */
+/*   Updated: 2024/09/18 20:57:06 by Paradis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/Harl.hpp"
-
+#include <iostream>
 int main(void)
 {
+    std::string debugLevel = "DEBUG";
+    std::string infoLevel = "INFO";
+    std::string warningLevel = "WARNING";
+    std::string errorLevel = "ERROR";
+    std::string somethingLevel = "SOMETHING";
+    Harl        harl;
+
+    std::cout << "DEUBG LEVEL: ";
+    harl.complain(debugLevel);
+    std::cout << "INFO LEVEL: ";
+    harl.complain(infoLevel);
+    std::cout << "WARNING LEVEL: ";
+    harl.complain(warningLevel);
+    std::cout << "ERROR LEVEL: ";
+    harl.complain(errorLevel);
+    std::cout << "ANYTHING LEVEL: ";
+    harl.complain(somethingLevel);
     return 0;
 }
