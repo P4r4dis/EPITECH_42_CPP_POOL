ea419285a63d9e2974aa252abd2e2e286fdae035
P4r4dis <adil.d.pro@gmail.com>
2024-09-19 19:52:35 +0200

    [ADD] - implementation of main function added

--- CPP_01/42/M01/ex11/bin/main.cpp
+++ CPP_01/42/M01/ex11/bin/main.cpp
@@ -6,30 +6,23 @@
 /*   By: Paradis <adil.d.pro@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 21:12:48 by Paradis           #+#    #+#             */
-/*   Updated: 2024/09/18 20:57:06 by Paradis          ###   ########.fr       */
+/*   Updated: 2024/09/19 19:49:36 by Paradis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/Harl.hpp"
 #include <iostream>
-int main(void)
-{
-    std::string debugLevel = "DEBUG";
-    std::string infoLevel = "INFO";
-    std::string warningLevel = "WARNING";
-    std::string errorLevel = "ERROR";
-    std::string somethingLevel = "SOMETHING";
-    Harl        harl;
 
-    std::cout << "DEUBG LEVEL: ";
-    harl.complain(debugLevel);
-    std::cout << "INFO LEVEL: ";
-    harl.complain(infoLevel);
-    std::cout << "WARNING LEVEL: ";
-    harl.complain(warningLevel);
-    std::cout << "ERROR LEVEL: ";
-    harl.complain(errorLevel);
-    std::cout << "ANYTHING LEVEL: ";
-    harl.complain(somethingLevel);
+int main(int ac, char **av)
+{
+    if (ac != 2 )
+        std::cerr   << "Error: level is unknown." << std::endl
+                    << "Choose between: DEBUG, INFO, WARNING, ERROR."
+                    << std::endl;
+    else
+    {
+        Harl    harl;
+        harl.dispatcher(av[1]);
+    }
     return 0;
 }
