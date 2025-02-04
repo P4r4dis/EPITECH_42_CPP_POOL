f0684bb0d910ec11958f323e459bc3f38776947f
P4r4dis <adil.d.pro@gmail.com>
2025-02-04 13:14:01 +0100

    [ADD] - implementation of main function added

--- CPP_01/EPITECH/D07/ex06/bin/main.cpp
+++ CPP_01/EPITECH/D07/ex06/bin/main.cpp
@@ -5,12 +5,19 @@
 ** Login   <Adil Denia>
 **
 ** Started on  Thu Jan 30 5:06:48 PM 2025 Paradis
-** Last update Fri Jan 30 5:07:00 PM 2025 Paradis
+** Last update Wed Feb 4 1:11:47 PM 2025 Paradis
 */
 
 #include "../include/Skat.hpp"
+#include <iostream>
 
 int main(void)
 {
+    Skat s("Junior", 5);
+    
+    std::cout << "Soldier " << s.name() << std::endl;
+    s.status();
+    s.useStimPaks(); 
+
     return 0;
 }
