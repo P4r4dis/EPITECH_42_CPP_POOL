fdd76773a6e770c60023ab5045b16dd7f33a5ec2
P4r4dis <adil.d.pro@gmail.com>
2025-02-04 21:10:49 +0100

    [ADD] - main function added

--- CPP_01/EPITECH/D07/ex07/bin/main.cpp
+++ CPP_01/EPITECH/D07/ex07/bin/main.cpp
@@ -5,14 +5,17 @@
 ** Login   <Adil Denia>
 **
 ** Started on  Tue Feb 4 1:25:38 PM 2025 Paradis
-** Last update Wed Feb 4 1:25:40 PM 2025 Paradis
+** Last update Wed Feb 4 9:09:57 PM 2025 Paradis
 */
 
-
+#include <iostream>
+#include "../include/KoalaBot.hpp"
 
 int main(void)
 {
+    KoalaBot kb;
 
-
+    std::cout << std::boolalpha << kb.status() << std::endl;
+    kb.informations();
     return 0;
 }
