19627317948794c3cdb124539d55204f4b1ee21f
P4r4dis <adil.d.pro@gmail.com>
2025-02-11 21:30:52 +0100

    [ADD] - unit test and main function implementation added

--- CPP_01/EPITECH/D07/ex09/bin/main.cpp
+++ CPP_01/EPITECH/D07/ex09/bin/main.cpp
@@ -5,12 +5,21 @@
 ** Login   <Adil Denia>
 **
 ** Started on  Mon Feb 10 7:32:31 PM 2025 Paradis
-** Last update Tue Feb 10 7:32:35 PM 2025 Paradis
+** Last update Wed Feb 11 9:27:58 PM 2025 Paradis
 */
 
-
+#include "../include/Phaser.hpp"
+#include <iostream>
 
 int     main(void)
 {
+    Phaser p(5, Phaser::ROCKET);
+
+    p.fire();
+    p.reload();
+
+    std::cout << "Firing all ammunitions" << std::endl;
+    while (p.getCurrentAmmos() > 0)
+        p.fire();
     return 0;
 }
