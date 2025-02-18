59c1b1010753ae5c4927149ae19e42512f80acd4
P4r4dis <adil.d.pro@gmail.com>
2025-02-18 21:16:21 +0100

    [ADD] - units tests and Copy CTOR + assigments operator implementation added

--- CPP_02/42/M02/ex00/source/Fixed.cpp
+++ CPP_02/42/M02/ex00/source/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: Paradis <adil.d.pro@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 20:19:18 by Paradis           #+#    #+#             */
-/*   Updated: 2025/02/18 20:20:53 by Paradis          ###   ########.fr       */
+/*   Updated: 2025/02/18 21:11:22 by Paradis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,24 @@ Fixed::Fixed(void)  : _fixedPointValue(0)
     std::cout << "Default constructor called" << std::endl;
 }
 
+Fixed::Fixed(const Fixed &src)  : _fixedPointValue(src._fixedPointValue)
+{
+    std::cout << "Copy constructor called" << std::endl;
+}
+
 Fixed::~Fixed(void)
 {
     std::cout << "Destructor called" << std::endl;
+}
+
+
+Fixed               &Fixed::operator=(const Fixed &src)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &src)
+        this->_fixedPointValue = src._fixedPointValue;
+
+    return *this;
 }
 
 int     Fixed::getRawBits(void) const
--- CPP_02/42/M02/ex00/test/bin/test_main.cpp
+++ CPP_02/42/M02/ex00/test/bin/test_main.cpp
@@ -6,7 +6,7 @@
 /*   By: Paradis <adil.d.pro@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 19:58:37 by Paradis           #+#    #+#             */
-/*   Updated: 2025/02/18 20:18:41 by Paradis          ###   ########.fr       */
+/*   Updated: 2025/02/18 21:15:06 by Paradis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,6 +70,68 @@ Test(Fixed_class, Test_getRawBits_func_prints_msg_to_stdout,
     cr_assert_stdout_eq_str(
         "Default constructor called\n"
         "getRawBits member function called\n"
+        "Destructor called\n"
+    );
+}
+
+Test(Fixed_class, Test_Copy_CTOR_isDefined, .init = redirect_all_stdout)
+{
+    Fixed a;
+    Fixed b(a);
+    cr_assert_not_null(&b);
+}
+
+Test(Fixed_class, Test_Copy_CTOR_copy_instance,
+.init = redirect_all_stdout)
+{
+    Fixed a;
+    Fixed b(a);
+    cr_assert(b.getRawBits() == 0);
+    cr_assert(b.getRawBits() == a.getRawBits());
+}
+
+Test(Fixed_class, Test_assigment_operator,
+.init = redirect_all_stdout)
+{
+    Fixed a;
+    Fixed b;
+
+    b = a;
+    cr_assert(b.getRawBits() == a.getRawBits());
+}
+
+Test(Fixed_class, Test_assigment_operator_prints_msg_to_stdout,
+.init = redirect_all_stdout)
+{
+    {
+        Fixed a;
+        Fixed b;
+
+        b = a;
+        cr_assert(b.getRawBits() == a.getRawBits());
+    }
+    cr_assert_stdout_eq_str(
+        "Default constructor called\n"
+        "Default constructor called\n"
+        "Copy assignment operator called\n"
+        "getRawBits member function called\n"
+        "getRawBits member function called\n"
+        "Destructor called\n"
+        "Destructor called\n"
+    );
+}
+
+Test(Fixed_class, Test_CopyCTOR_and_CTOR_and_DTOR_prints_msg_to_stdout,
+.init = redirect_all_stdout)
+{
+    {
+        Fixed a;
+        Fixed b(a);
+    }
+    cr_assert_stdout_eq_str(
+        "Default constructor called\n"
+        "Copy constructor called\n"
+        "Destructor called\n"
         "Destructor called\n"
     );
 }
