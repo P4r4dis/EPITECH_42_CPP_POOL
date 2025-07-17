817940af07afc99d2654932ccb450cb8669870c6
P4r4dis <adil.d.pro@gmail.com>
2025-07-17 18:53:13 +0200

    [ADD] - Utils::printChar
    - Take a string as parameter and if its a char convert the string to int, float and double then display msg on stdout.
    - printChar display specific msg for non displayable character.

--- CPP_06/42/M06/ex00/source/Utils.cpp
+++ CPP_06/42/M06/ex00/source/Utils.cpp
@@ -6,7 +6,7 @@
 /*   By: Paradis <adil.d.pro@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 20:37:18 by Paradis           #+#    #+#             */
-/*   Updated: 2025/07/17 18:11:50 by Paradis          ###   ########.fr       */
+/*   Updated: 2025/07/17 18:52:50 by Paradis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,32 +73,29 @@ bool         Utils::isPrintable(const std::string &str)
     return std::isprint(static_cast<unsigned char>(str.at(0)));
 }
 
-// void        Utils::printChar(const std::string &str)
-// {
-//     std::cout << "char: ";
-//     if (isChar(std::string(1,str[0])))
-//     {
-//         if (isPseudoLiteral(str))
-//         {
-//             std::cout << "impossible" << std::endl;
-//         }
-//         if (isPrintable(std::string(1,str[0])))
-//             std::cout << '\'' << str << '\'' << std::endl;
-//         else
-//             std::cout << "Non displayable" << std::endl;
+void        Utils::printChar(const std::string &str)
+{
+    std::cout << "char: ";
+    if (isChar(std::string(1,str[0])))
+    {
+        if (isPrintable(std::string(1,str[0])))
+            std::cout << '\'' << str << '\'' << std::endl;
+        else
+            std::cout << "Non displayable" << std::endl;
 
-//         std::cout   << "int: " << static_cast<int>(str[0]) << std::endl;
-//         std::cout   << "float: " << static_cast<float>(str[0]);
-//         if (static_cast<int>(str[0]) ==
-//             static_cast<float>(str[0]))
-//             std::cout << ".0f" << std::endl;
-//         else
-//             std::cout << "f" << std::endl;
-//         std::cout   << "double: " << static_cast<double>(str[0]);
-//         if (static_cast<int>(str[0]) == static_cast<double>(str[0]))
-//             std::cout << ".0" << std::endl;
-//     }
-// }
+        std::cout   << "int: " << static_cast<int>(str[0]) << std::endl;
+        std::cout   << "float: " << static_cast<float>(str[0]);
+        if (static_cast<int>(str[0]) == static_cast<float>(str[0]))
+            std::cout << ".0f" << std::endl;
+        // else
+        // {
+        //     std::cout << "f" << std::endl;
+        // }
+        std::cout   << "double: " << static_cast<double>(str[0]);
+        if (static_cast<int>(str[0]) == static_cast<double>(str[0]))
+            std::cout << ".0" << std::endl;
+    }
+}
 
 void            Utils::printLiteral(const std::string &str)
 {
