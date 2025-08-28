ea9c123932a43cb4bfe4fc136e9f260db56ead9a
P4r4dis <adil.d.pro@gmail.com>
2025-08-28 20:23:48 +0200

    [ADD] - main function
    - main function is added.
    - units test for all functions added.

--- CPP_07/42/M07/ex00/bin/main.cpp
+++ CPP_07/42/M07/ex00/bin/main.cpp
@@ -6,14 +6,14 @@
 /*   By: Paradis <adil.d.pro@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/28 17:12:37 by Paradis           #+#    #+#             */
-/*   Updated: 2025/08/28 17:14:50 by Paradis          ###   ########.fr       */
+/*   Updated: 2025/08/28 19:59:37 by Paradis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "../include/whatever.hpp"
 
-int     main(int ac, char **av)
+int     main(void)
 {
     int a = 2;
     int b = 3;
@@ -21,8 +21,8 @@ int     main(int ac, char **av)
     ::swap( a, b );
 
     std::cout << "a = " << a << ", b = " << b << std::endl;
-    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::cout << "min(a, b) = " << ::min( a, b ) << std::endl;
+    std::cout << "max(a, b) = " << ::max( a, b ) << std::endl;
 
     std::string c = "chaine1";
     std::string d = "chaine2";
@@ -30,8 +30,8 @@ int     main(int ac, char **av)
     ::swap(c, d);
 
     std::cout << "c = " << c << ", d = " << d << std::endl;
-    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    std::cout << "min(c, d) = " << ::min( c, d ) << std::endl;
+    std::cout << "max(c, d) = " << ::max( c, d ) << std::endl;
 
     return 0;
 }
