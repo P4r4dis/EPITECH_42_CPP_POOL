7fa6f233fbfb96281d45f27b86362735da4d42aa
P4r4dis <adil.d.pro@gmail.com>
2025-08-28 19:54:02 +0200

    [ADD] - min
    - Compares the two values passed in its arguments and returns the smallest one.
    - If the two of them are equal, then it returns the second one.

--- CPP_07/42/M07/ex00/include/whatever.hpp
+++ CPP_07/42/M07/ex00/include/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: Paradis <adil.d.pro@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/28 17:30:12 by Paradis           #+#    #+#             */
-/*   Updated: 2025/08/28 17:54:37 by Paradis          ###   ########.fr       */
+/*   Updated: 2025/08/28 19:50:34 by Paradis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,21 @@
 #define                 __WHATEVER_HPP__
 
 template <typename T>
-void    swap(T &a, T &b)
+void        swap(T &a, T &b)
 {
     const T tmp = a;
 
     a = b;
     b = tmp;
 }
+
+template <typename T>
+const T     &min(const T &a, const T &b)
+{
+    if (a < b)
+        return a;
+    return b;
+}
+
 
 #endif //               !_WHATEVER_HPP__
--- CPP_07/42/M07/ex00/test/bin/test_main.cpp
+++ CPP_07/42/M07/ex00/test/bin/test_main.cpp
@@ -6,7 +6,7 @@
 /*   By: Paradis <adil.d.pro@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/28 17:16:13 by Paradis           #+#    #+#             */
-/*   Updated: 2025/08/28 19:19:17 by Paradis          ###   ########.fr       */
+/*   Updated: 2025/08/28 19:53:46 by Paradis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,6 +81,71 @@ Test(swap, TEST_swap_2_values_of_2_arguments_with_any_type,
     }
 }
 
+Test(min, TEST_return_the_smallest_values_of_2_arguments,
+    .init = redirect_all_stdout)
+{
+    {
+        int     a = 2;
+        int     b = 3;
+        int     result = 0;
+
+        cr_assert(a == 2);
+        cr_assert(b == 3);
+
+        result = ::min(a, b);
+
+        cr_assert(result == a);
+    }
+}
+
+Test(min, TEST_return_the_second_values_of_2_arguments_if_the_values_are_equal,
+    .init = redirect_all_stdout)
+{
+    {
+        int     a = 2;
+        int     b = 2;
+        int     result = 0;
+
+        cr_assert(a == 2);
+        cr_assert(b == 2);
+
+        result = ::min(a, b);
+
+        cr_assert(result == b);
+    }
+}
+
+Test(min, TEST_return_the_smallest_values_of_2_arguments_with_any_type,
+    .init = redirect_all_stdout)
+{
+    {
+        int     a = 2;
+        int     b = 3;
+        
+        cr_assert(a == 2);
+        cr_assert(b == 3);
+
+        cr_assert(min(a, b) == a);
+
+        char    c = 'c';
+        char    d = 'd';
+
+        cr_assert(c == 'c');
+        cr_assert(d == 'd');
+
+        cr_assert(min(c, d) == c);
+
+
+        std::string    str1 = "Hello";
+        std::string    str2 = "World";
+        std::string    result = ::min(str1, str2);
+
+        cr_assert(str1 == "Hello");
+        cr_assert(str2 == "World");
+        cr_assert(result == str1);
+    }
+}
+
 ///////////////////////////////////////////////////////////////////////////////
 //                            TEST main                                      //
 ///////////////////////////////////////////////////////////////////////////////
@@ -93,7 +158,7 @@ Test(main, Test_main, .init = redirect_all_stdout)
         ::swap( a, b );
 
         std::cout << "a = " << a << ", b = " << b << std::endl;
-//         std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+        std::cout << "min(a, b) = " << ::min( a, b ) << std::endl;
 //         std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 
         std::string c = "chaine1";
@@ -102,16 +167,16 @@ Test(main, Test_main, .init = redirect_all_stdout)
         ::swap(c, d);
 
         std::cout << "c = " << c << ", d = " << d << std::endl;
-//         std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+        std::cout << "min(c, d) = " << ::min( c, d ) << std::endl;
 //         std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
     }
     cr_assert_stdout_eq_str
     (
         "a = 3, b = 2\n"
-        // "min(a, b) = 2\n"
+        "min(a, b) = 2\n"
         // "max(a, b) = 3\n"
         "c = chaine2, d = chaine1\n"
-        // "min(c, d) = chaine1\n"
+        "min(c, d) = chaine1\n"
         // "max(c, d) = chaine2\n"
     );
 }
