246fab712bb0de4840d7d9b09cca4608fed1d0bb
P4r4dis <adil.d.pro@gmail.com>
2025-09-08 20:36:48 +0200

    [ADD] - CTOR with parameter
    - Creates an array of n elements initialized by default.

--- CPP_07/42/M07/ex02/include/Array.hpp
+++ CPP_07/42/M07/ex02/include/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: Paradis <adil.d.pro@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/29 19:31:46 by Paradis           #+#    #+#             */
-/*   Updated: 2025/08/29 21:22:14 by Paradis          ###   ########.fr       */
+/*   Updated: 2025/09/08 20:35:48 by Paradis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,37 @@ class                    Array
 {
     public:
         Array(void);
-        // Array(unsigned int n);
+        Array(unsigned int n);
         // Array(const Array &copy);
         // Array           &operator=(const Array &copy);
         virtual ~Array(void);
 
         unsigned int    size(void) const;
+        T               operator[](unsigned int idx);
+
     private:
         const T         *_array;
         unsigned int    _n;
 };
+
+template <typename T>
+Array<T>::Array(void)  :    _n(0)
+{
+    _array = new T[_n];
+}
+
+template <typename T>
+Array<T>::Array(unsigned int n)  :  _n(n)
+{
+    _array = new T[_n] {};
+}
+
+template <typename T>
+Array<T>::~Array(void)
+{
+    if (_array)
+        delete [] _array;
+}
 
 template <typename T>
 unsigned int            Array<T>::size(void) const
@@ -38,16 +59,9 @@ unsigned int            Array<T>::size(void) const
 }
 
 template <typename T>
-Array<T>::Array(void)  :    _array(nullptr), _n(0)
+T                        Array<T>::operator[](unsigned int idx)
 {
-    _array = new T[_n];
+    return _array[idx];
 }
 
-template <typename T>
-
-Array<T>::~Array(void)
-{
-    if (_array)
-        delete [] _array;
-}
 #endif //               !_ARRAY_HPP__
--- CPP_07/42/M07/ex02/test/bin/test_main.cpp
+++ CPP_07/42/M07/ex02/test/bin/test_main.cpp
@@ -6,7 +6,7 @@
 /*   By: Paradis <adil.d.pro@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/29 19:33:07 by Paradis           #+#    #+#             */
-/*   Updated: 2025/08/29 21:23:39 by Paradis          ###   ########.fr       */
+/*   Updated: 2025/09/08 20:36:18 by Paradis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void redirect_all_stdout(void)
     cr_redirect_stderr();
 }
 
-Test(Array_constructor_with_no_parameter, TEST_is_defined,
+Test(Array, TEST_constructor_with_no_parameter_is_defined,
     .init = redirect_all_stdout)
 {
     Array<int>     array;
@@ -34,7 +34,7 @@ Test(Array_constructor_with_no_parameter, TEST_is_defined,
     cr_assert_not_null(&array);
 }
 
-Test(Array_constructor_with_no_parameter, TEST_should_create_an_empty_array,
+Test(Array, TEST__constructor_with_no_parameter_should_create_an_empty_array,
     .init = redirect_all_stdout)
 {
     Array<void *>     array;
@@ -42,6 +42,27 @@ Test(Array_constructor_with_no_parameter, TEST_should_create_an_empty_array,
     cr_assert(array.size() == 0);
 }
 
+Test(Array, TEST_constructor_with_parameter_is_defined,
+    .init = redirect_all_stdout)
+{
+    Array<int>     array(10);
+
+    cr_assert_not_null(&array);
+}
+
+Test(Array, TEST__constructor_with_parameter_create_array_of_N_elements,
+)//.init = redirect_all_stdout)
+{
+    Array<int>     array(10);
+
+    cr_assert(array.size() == 10);
+    
+    int *a = new int();
+    std::cout << *a << std::endl;
+
+    for (unsigned int i = 0; i < array.size(); ++i)
+        cr_assert(array[i] == 0);
+}
 ///////////////////////////////////////////////////////////////////////////////
 //                            TEST main                                      //
 ///////////////////////////////////////////////////////////////////////////////
